9988259097f0a0e68bcff3c31e0435591e177986
Laki59 <giniclazar49@gmail.com>
2025-02-21 22:06:28 +0100

    Search
    Index.php for searching champions and table pops up

---
+++ LolStats/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$conn = mysqli_connect("localhost","root","","projekat")
+        or die ("Couldn't connect");
+
+        ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" id="name">
+</body>
+</html>
